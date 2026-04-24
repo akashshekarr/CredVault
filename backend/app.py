b121941a5e1d2bd9f5b7dec1808de3fad85f99bd
Akash Shekar <akashshekar01@gmail.com>
2026-04-24 11:55:00 +0530

    Fix

--- backend/app.py
+++ backend/app.py
@@ -15,6 +15,8 @@ from authlib.integrations.flask_client import OAuth
 load_dotenv()
 
 app = Flask(__name__, template_folder="templates")
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.jinja_env.auto_reload = True
 app.secret_key = os.getenv("SECRET_KEY", "credsvault_5cnetwork_2024_xK9mP")
 
 oauth = OAuth(app)
